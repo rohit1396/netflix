5c95eb3505d8b0f3212a0291b7100d9b2dc71cca
Rohit _Gaikwad <rohitgaikwad726@gmail.com>
2022-08-26 14:11:52 +0530

    searchPagechange

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,11 +1,8 @@
 import React from "react";
-import axios from "./axios";
 import Navbar from "./Navbar";
 import "./SearchPage.css";
 
-const base_url = "https://image.tmdb.org/t/p/original/";
-
-const SearchPage = ({ title, img }) => {
+const SearchPage = () => {
   return (
     <div className="searchpage">
       <Navbar />
